393966d062b5fa6215a305b514cc237639b38411
Cameron Bisca <cjbisca@gmail.com>
2020-11-29 14:55:12 -0500

    commit tree 76f21234adeecb5db28fb0c13d8861bfdf04147d

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,38 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id  
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  def initialize(name, grade, id=nil)
+    @name = name 
+    @grade = grade 
+    @id = id 
+    
+    
+    def self.create_table
+      sql =  <<-SQL 
+        CREATE TABLE IF NOT EXISTS students (
+          id INTEGER PRIMARY KEY, 
+          name TEXT, 
+          grade INTEGER 
+          )
+          SQL
+      DB[:conn].execute(sql) 
+    end
+
+    def save
+      sql = <<-SQL
+        INSERT INTO songs (name, grade) 
+        VALUES (?, ?)
+      SQL
+  
+      DB[:conn].execute(sql, self.name, self.grade)
+  
+    end
+
+    def self.create(name:, grade:)
+      student = Student.new(name, grade)
+      student.save
+      student
+    end
   
 end
